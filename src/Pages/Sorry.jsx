8643135981d28b139cd14340ee2f179ff985d55d
michael-ot <michaelotusanya@gmail.com>
2024-10-30 13:07:52 +0100

    xlink chnage

--- src/Pages/Sorry.jsx
+++ src/Pages/Sorry.jsx
@@ -13,14 +13,14 @@ const Sorry = () => {
           <div className="absolute flex flex-col items-center top-[90px]">
           
             <p className="text-white text-1xl mb-[10px]"> Sorry!!  You have no money</p>
-            {/* <Link to={"/login"}> */}
+            <Link to={"/amount"}>
               <button
                 className="w-full text-black my-[20px] py-2 px-4 rounded-md bg-100% h-[40px] bg-no-repeat bg-center"
                 style={{ backgroundImage: "url('/images/ExtraLongButton.png')" }}
               >
                 Buy More?
               </button>
-            {/* </Link> */}
+            </Link>
           </div>
         </div>
       </div>
