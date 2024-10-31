5fc0be5ee527e880d061c918e6b80d450da084de
michael-ot <michaelotusanya@gmail.com>
2024-10-31 22:54:41 +0100

    Updated coins

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     < Router>
       <Routes>
-      <Route path="/" element={<Amount/>} />
+      <Route path="/" element={<GameFrame/>} />
       <Route path="/game" element={<GameFrame />} />
         <Route path="/amount" element={<Amount />} />
         <Route path="/verify" element={<LoadingPage />} />
