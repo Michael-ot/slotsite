146dbf43f7e7ec597b1b468d91934e58ebcbf416
michael-ot <michaelotusanya@gmail.com>
2024-12-03 16:39:41 +0100

    update

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -276,8 +276,38 @@ export default function LandingPage() {
           }}
         >
           <div className="loader-container">
+          <div
+            className="absolute inset-0 bg-black/30 bg-opacity-30"
+            style={{ backdropFilter: "blur(4px)" }}
+            aria-hidden="true"
+          />
+
             <div className="spinner"></div>
           </div>
+
+          <style jsx>{`
+            .loader-container {
+              text-align: center;
+            }
+
+            .spinner {
+              width: 50px;
+              height: 50px;
+              border: 5px solid #222222;
+              border-top: 5px solid #ffea31;
+              border-radius: 50%;
+              animation: spin 1s linear infinite;
+            }
+
+            @keyframes spin {
+              0% {
+                transform: rotate(0deg);
+              }
+              100% {
+                transform: rotate(360deg);
+              }
+            }
+          `}</style>
         </div>
       )}
     </>
